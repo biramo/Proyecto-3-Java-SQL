1a40b40ff0cf012d49db41816004181615745013
Raül <467834.clot@fje.edu>
2026-04-20 18:33:03 +0200

    El archivo de conexión, los parámetros URL, USER y PASSWORD no los he llenado.

--- src/java/Controller/ConexionBD.java
+++ src/java/Controller/ConexionBD.java
@@ -1,4 +1,19 @@
 package Controller;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class ConexionBD {
+    private static final String URL = "";
+    private static final String USER = "";
+    private static final String PASSWORD = "";
+
+    public static Connection conexion() {
+        try{
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            System.out.println("Error de conexión: " + e.getMessage());
+            return null;
+        }
+    }
 }
