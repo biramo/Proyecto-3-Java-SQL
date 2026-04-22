71c371c80d71a71c22eee08f4f5f56d560290b7a
arnau <arnau.roy.ramos@gmail.com>
2026-04-22 19:15:41 +0200

    Interfaz Cliente:  -Funcion calcularDeuda() no implementada Cliente:  Cliente acabado sin la implementacion calcularDeuda()

--- src/java/model/Cliente.java
+++ src/java/model/Cliente.java
@@ -1,4 +1,46 @@
 package model;
 
-public class Cliente {
+import model.Interfaces.InCliente;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public class Cliente extends Persona implements InCliente {
+
+    //Atributos
+    private LocalDate fechaNacimiento;
+    private double deudaPendiente;
+
+    //Constructor
+    public Cliente(final String dni, final String nombre, final String telefono, final String apellidos, final String email, final LocalDate fechaNacimiento, final double deudaPendiente) {
+        super(dni, nombre, telefono, apellidos, email);
+        this.fechaNacimiento = fechaNacimiento;
+        this.deudaPendiente = deudaPendiente;
+    }
+
+    //Metodo devuelve texto con los datos del cliente
+    public String mostrarCliente() {
+        return "Cliente: Dni: " + getDni() + ", Nombre: " + getNombre() + ", Apellidos: " + getApellidos() + calcularEdad();
+    }
+
+    //Calcula la edad con la fecha
+    public int calcularEdad() {
+        LocalDate hoy = LocalDate.now();
+        if (this.fechaNacimiento != null) {
+            return Period.between(this.fechaNacimiento, hoy).getYears();
+        }
+        return 0;
+    }
+
+    //-------------------------Metodo en STANBY-------------------------
+    //public double calcularDeuda(){}
+
+    //To string
+    @Override
+    public String toString() {
+        return super.toString() + "Cliente{" +
+                "fechaNacimiento=" + fechaNacimiento +
+                ", deudaPendiente=" + deudaPendiente +
+                '}';
+    }
 }
--- src/java/model/Interfaces/InCliente.java
+++ src/java/model/Interfaces/InCliente.java
@@ -3,5 +3,6 @@ package model.Interfaces;
 public interface InCliente {
     public int calcularEdad();
 
-    public double calcularDeuda();
+    //-------------------------Metodo en STANBY-------------------------
+    //public double calcularDeuda();
 }
