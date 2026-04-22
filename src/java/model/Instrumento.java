7f80cd09a1a1d7e0d4bbb46304a12e83a63a7202
Raül <467834.clot@fje.edu>
2026-04-22 19:53:49 +0200

    Setters de: id, marca y modelo eliminados en Instrumento

--- src/java/model/Instrumento.java
+++ src/java/model/Instrumento.java
@@ -17,15 +17,9 @@ public class Instrumento implements InAlquilable {
     //Getters y Setters
     public int getId() { return id; }
 
-    public void setId(int id) { this.id = id; }
-
     public String getMarca() { return marca; }
 
-    public void setMarca(String marca) { this.marca = marca; }
-
     public String getModelo() { return modelo; }
-
-    public void setModelo(String modelo) { this.modelo = modelo;    }
 
     public double getPrecioDia() { return precioDia; }
 
