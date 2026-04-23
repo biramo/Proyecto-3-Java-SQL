f5397d3904077f0725034148e5087bb919fec0b4
Raül <467834.clot@fje.edu>
2026-04-23 19:05:06 +0200

    AccesoAdministrador: Movido el codigo a AccesoAdministrador.java, cambiada la variable "codigo" a final y cambiadas de lugar el Boolean y el int.

--- src/java/model/AccesoAdministrador.java
+++ src/java/model/AccesoAdministrador.java
@@ -1,4 +1,34 @@
 package model;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class AccesoAdministrador {
+    private static final String codigo = "clotfje";
+    Scanner sc = new Scanner(System.in);
+    String respuesta;
+
+    public void validador(int intentos) {
+        do {
+            try {
+                System.out.println("Introduce el código de Administrador");
+                respuesta = sc.nextLine();
+
+                if (respuesta == codigo) {
+                    System.out.println("Acceso Validado");
+                    boolean accesoValido = true;
+                } else {
+                    System.out.println("Acceso Denegado");
+                    intentos++;
+                }
+                sc.nextLine();
+                if(intentos == 3){
+                    System.out.println("Has superado el número de intentos");
+                    break;
+                }
+            }catch(InputMismatchException e){
+                System.out.println("Tienes que escribir un código con símbolos válidos, " + e.getMessage());
+            }
+        }while(respuesta != codigo);
+    }
 }
