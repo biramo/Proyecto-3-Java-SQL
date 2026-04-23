8daf9b99a09e133062fa2bed5cb591030699cac6
Raül <467834.clot@fje.edu>
2026-04-23 18:55:30 +0200

    MenuAcceso: Hecha la lógica, falta encontrar errores y hacer pruebas.

--- src/java/Menu/MenuAcceso.java
+++ src/java/Menu/MenuAcceso.java
@@ -1,4 +1,35 @@
 package Menu;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class MenuAcceso {
+    private static String codigo = "clotfje";
+    private boolean accesoValido;
+    Scanner sc = new Scanner(System.in);
+    String respuesta;
+
+    public void validador(int intentos) {
+        do {
+            try {
+                System.out.println("Introduce el código de Administrador");
+                respuesta = sc.nextLine();
+
+                if (respuesta == codigo) {
+                    System.out.println("Acceso Validado");
+                    accesoValido = true;
+                } else {
+                    System.out.println("Acceso Denegado");
+                    intentos++;
+                }
+                sc.nextLine();
+                if(intentos == 3){
+                    System.out.println("Has superado el número de intentos");
+                    break;
+                }
+            }catch(InputMismatchException e){
+                System.out.println("Tienes que escribir un código con símbolos válidos" + e.getMessage());
+            }
+        }while(respuesta != codigo);
+    }
 }
