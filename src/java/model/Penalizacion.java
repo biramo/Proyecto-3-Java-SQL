17543401bb49911451f340afa463fc26a4d58bcd
Raül <467834.clot@fje.edu>
2026-05-04 19:04:32 +0200

    Modelos: Optimizadas líneas para quitar advertencias, corregidas faltas de ortografía menores.

--- src/java/model/Penalizacion.java
+++ src/java/model/Penalizacion.java
@@ -49,10 +49,10 @@ public class Penalizacion {
     public String mostrarPenalizacion() {
         StringBuilder sb = new StringBuilder();
         //StringBuilder para no gastar tanta memoria
-        sb.append("Penalizacion: \n");
+        sb.append("Penalización: \n");
         sb.append("Motivo: \n");
         sb.append(motivo);
-        //un ternario por si no hay desperfecto y solo es por retraso la penalizacion
+        //un ternario por si no hay desperfecto y solo es por retraso la penalización
         sb.append(
                 desperfecto != TipoDesperfecto.NINGUNO
                         ? "Desperfecto: " + desperfecto + "\n"
@@ -65,7 +65,7 @@ public class Penalizacion {
 
     @Override
     public String toString() {
-        return "Penalizacion{" +
+        return "Penalización{" +
                 "id=" + id +
                 ", motivo='" + motivo + '\'' +
                 ", importe=" + importe +
